1291cb422e9b1fad0d48e094f6258931ad2dd04f
Yang <zhy0216@gmail.com>
2022-03-15 16:23:07 +0800

    add mini-immer basic implmentation

--- mini-immer/src/index.ts
+++ mini-immer/src/index.ts
@@ -1,6 +1,64 @@
 
+type Action = {name: string | symbol, value: any, type: "set"} | { name: string | symbol, next: ObjectProxy<any>, type: "get"}
+
+class ObjectProxy<T extends object> {
+  baseObj: T
+  revokeProxy?: { proxy: T; revoke: () => void; }
+  actions: Action[]
+  constructor(baseObj: T) {
+    this.baseObj = baseObj
+    this.actions = []
+    const self = this
+    const handler: ProxyHandler<T> = {
+      get: function(target, name) {
+        const nextProxy = new ObjectProxy(target[name])
+        self.actions.push({name, next: nextProxy, type: "get"})
+
+        return nextProxy.proxy
+      },
+
+      set: function (target, name, value): boolean {
+        self.actions.push({name, value, type: "set"})
+
+        return true
+      }
+    }
+
+    this.revokeProxy = Proxy.revocable<T>(baseObj, handler)
+  }
+
+  get proxy(): T {
+    return this.revokeProxy.proxy
+  }
+
+  revoke = () => {
+
+  }
+
+  build = (): T => {
+    let newObj = {...this.baseObj}
+    for(const effect of this.actions) {
+      if(effect.type === "set") {
+        const {name, value} = effect
+        newObj = {...this.baseObj, [name]: value}
+      } else if(effect.type === "get") {
+        const {name, next} = effect
+        newObj = {...this.baseObj, [name]: next.build()}
+      }
+    }
+
+    return newObj
+  }
+}
 
 
-export const product = () => {
+export function produce<T extends object>(baseObj: T, changeFunc: (d: T) => void): T {
+  const proxyObj = new ObjectProxy<T>(baseObj)
 
+  changeFunc(proxyObj.proxy)
+  const newObj = proxyObj.build()
+
+  proxyObj.revoke()
+
+  return newObj
 }
--- mini-immer/tests/immer.test.ts
+++ mini-immer/tests/immer.test.ts
@@ -1,7 +1,28 @@
-import {product} from "../src"
+import {produce} from "../src"
+import * as assert from "assert"
 
-describe('testing index file', () => {
-  test('empty string should result in zero', () => {
-    expect(0).toBe(0);
+describe('testing immer', () => {
+  test('produce a new object', () => {
+    const a = {b: 1}
+    const na = produce(a, draft => {
+      draft.b = 2
+    })
+    assert(a !== na)
+    assert(a.b === 1)
+    assert(na.b === 2)
+  });
+
+  test('produce a new nested object', () => {
+    const a = {b: {c: 1}, b1: {}}
+    const na = produce(a, draft => {
+      // console.log("##### draft.b.c: ", draft.b.c)
+      draft.b.c = 2
+    })
+
+    assert(a !== na)
+    assert(a.b !== na.b)
+    assert(a.b1 === na.b1)
+    assert(a.b.c === 1)
+    assert(na.b.c === 2)
   });
 });
